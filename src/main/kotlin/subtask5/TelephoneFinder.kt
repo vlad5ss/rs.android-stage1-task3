0adab005ffe206e8a681bf83484c4fa6fff7cfae
vlad5ss <vlad5grup@gmail.com>
2020-05-06 00:03:00 +0400

    add: Telephonenumber

--- src/main/kotlin/subtask5/TelephoneFinder.kt
+++ src/main/kotlin/subtask5/TelephoneFinder.kt
@@ -1,11 +1,40 @@
 package subtask5
 
-import kotlin.reflect.KClass
-
 class TelephoneFinder {
 
     // TODO: Complete the following function
     fun findAllNumbersFromGivenNumber(number: String): Array<String>? {
-        throw NotImplementedError("Not implemented")
+        if (number.toInt() < 0) {
+            return null;
+        }
+        val result = ArrayList<String>();
+
+        val allNumbers = mapOf(
+            '0' to arrayOf(8),
+            '1' to arrayOf(2, 4),
+            '2' to arrayOf(1, 3, 5),
+            '3' to arrayOf(2, 6),
+            '4' to arrayOf(1, 5, 7),
+            '5' to arrayOf(2, 4, 6, 8),
+            '6' to arrayOf(3, 5, 9),
+            '7' to arrayOf(4, 8),
+            '8' to arrayOf(0, 5, 7, 9),
+            '9' to arrayOf(6, 8)
+        )
+
+        for (i in number.indices) {
+            val wrongNumber = number[i];
+            val arrayOfNeigNumbers = allNumbers[wrongNumber];
+
+            if (!allNumbers.containsKey(wrongNumber)) {
+                return null;
+            }
+
+            for (neigIndex in arrayOfNeigNumbers!!.indices) {
+                result.add(number.replaceRange(i, i + 1, arrayOfNeigNumbers[neigIndex].toString()));
+            }
+        }
+
+        return result.toTypedArray()
     }
 }
